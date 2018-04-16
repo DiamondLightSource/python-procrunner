26b1626722c0b75aa3f58fd12d6e26e4305b5a9e
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-04-16 11:52:17 +0100

    Add system test for procrunner

---
+++ tests/test_procrunner_system.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import, division, print_function
+
+import procrunner
+import os
+
+def test_simple_command_invocation():
+  if os.name == 'nt':
+    command = ['cmd.exe', '/c', 'echo', 'hello']
+  else:
+    command = ['echo', 'hello']
+
+  result = procrunner.run(command)
+
+  assert result['exitcode'] == 0
+  assert result['stdout'].strip() == 'hello'
+  assert result['stderr'] == ''
