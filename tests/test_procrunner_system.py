65d2e2af92fb5f60621f08cdbce3fe3603a39e7e
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-05-05 22:32:04 +0100

    test for broken encoding

--- tests/test_procrunner_system.py
+++ tests/test_procrunner_system.py
@@ -14,3 +14,11 @@ def test_simple_command_invocation():
   assert result['exitcode'] == 0
   assert result['stdout'] == b'hello' + os.linesep.encode('utf-8')
   assert result['stderr'] == b''
+
+def test_input_encoding():
+  command = ['python', '-c', 'import sys; sys.stdout.write("".join(chr(x) for x in '
+             '(0x74,0x65,0x73,0x74,0xa0,0x50,0x73,0x74,0x72,0x69,0x6e,0x67,0x0a)'
+             '))']
+  result = procrunner.run(command)
+
+  assert result['exitcode'] == 0
