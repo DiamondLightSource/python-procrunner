97a0a4a9b56e08aae007afafb659611182671f37
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-05-05 22:35:22 +0100

    make test portable

--- tests/test_procrunner_system.py
+++ tests/test_procrunner_system.py
@@ -1,7 +1,9 @@
 from __future__ import absolute_import, division, print_function
 
-import procrunner
 import os
+import sys
+
+import procrunner
 
 def test_simple_command_invocation():
   if os.name == 'nt':
@@ -16,7 +18,7 @@ def test_simple_command_invocation():
   assert result['stderr'] == b''
 
 def test_input_encoding():
-  command = ['python', '-c', 'import sys; sys.stdout.write("".join(chr(x) for x in '
+  command = [sys.executable, '-c', 'import sys; sys.stdout.write("".join(chr(x) for x in '
              '(0x74,0x65,0x73,0x74,0xa0,0x50,0x73,0x74,0x72,0x69,0x6e,0x67,0x0a)'
              '))']
   result = procrunner.run(command)
