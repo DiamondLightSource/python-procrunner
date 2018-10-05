8acfc2fb382002b1810850c367c3103d957f4890
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-10-05 13:18:00 +0100

    Bump version: 0.7.1 → 0.7.2

--- procrunner/__init__.py
+++ procrunner/__init__.py
@@ -53,7 +53,7 @@ from threading import Thread
 
 __author__ = """Markus Gerstel"""
 __email__ = 'scientificsoftware@diamond.ac.uk'
-__version__ = '0.7.1'
+__version__ = '0.7.2'
 
 logger = logging.getLogger('procrunner')
 logger.addHandler(logging.NullHandler())
