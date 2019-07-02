902879fd4b13ec79b2a2bc7715446d35f1cdf45f
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-07-02 08:32:48 +0100

    Drop Python 3.4 support

--- setup.py
+++ setup.py
@@ -12,9 +12,7 @@ with open("HISTORY.rst") as history_file:
 
 requirements = [
     "six",
-    # PyWin32 is only supported on 2.7 and 3.5+
-    'pywin32; sys_platform=="win32" and python_version=="2.7"',
-    'pywin32; sys_platform=="win32" and python_version>="3.5"',
+    'pywin32; sys_platform=="win32"',
 ]
 
 setup_requirements = []
@@ -36,7 +34,6 @@ setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
