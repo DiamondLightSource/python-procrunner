ac1b70da2e938b3e00a84fa6801ee7beaa59112e
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2020-06-23 19:59:10 +0100

    drop pypy support

--- setup.py
+++ setup.py
@@ -35,8 +35,6 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     description="Versatile utility function to run external processes",
