ac9af008fa3adb46448038bc0a2a15ea8fa991f1
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2020-09-05 20:39:31 +0100

    Bump version: 2.0.0 → 2.1.0

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/DiamondLightSource/python-procrunner",
-    version="2.0.0",
+    version="2.1.0",
     zip_safe=False,
 )
