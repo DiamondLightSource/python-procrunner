efda47208936e11559d291a6a7d444754845c93f
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2022-01-27 08:49:16 +0000

    Change package to src layout

--- setup.py
+++ setup.py
@@ -1,7 +1,4 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from setuptools import find_packages, setup
+from setuptools import setup
 
 with open("README.rst") as readme_file:
     readme = readme_file.read()
@@ -9,24 +6,7 @@ with open("README.rst") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = []
-
-setup_requirements = []
-
-test_requirements = ["pytest"]
-
 setup(
-    author="Markus Gerstel",
-    author_email="scientificsoftware@diamond.ac.uk",
-    install_requires=requirements,
     long_description=readme + "\n\n" + history,
-    include_package_data=True,
     keywords="procrunner",
-    packages=find_packages(include=["procrunner"]),
-    python_requires=">=3.6",
-    setup_requires=setup_requirements,
-    test_suite="tests",
-    tests_require=test_requirements,
-    url="https://github.com/DiamondLightSource/python-procrunner",
-    zip_safe=False,
 )
